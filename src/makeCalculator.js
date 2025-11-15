fe45a4b6b079bd619f76f2d815ab2a8242a1be00
Maksym Halushko <gallikillag@gmail.com>
2025-11-15 15:17:58 +0200

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculate = {
+    result: 0,
+    add(n) {
+      calculate.result += n;
+
+      return this;
+    },
+    subtract(n) {
+      calculate.result -= n;
+
+      return this;
+    },
+    multiply(n) {
+      calculate.result *= n;
+
+      return this;
+    },
+    divide(n) {
+      calculate.result /= n;
+
+      return this;
+    },
+    reset() {
+      calculate.result = 0;
+
+      return this;
+    },
+    operate(callback, n) {
+      callback(n);
+
+      return this;
+    },
+  };
+
+  return calculate;
 }
 
 module.exports = makeCalculator;
